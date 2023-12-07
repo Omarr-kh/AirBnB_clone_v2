71b329624c8cfe5467f34e7a942a8bccd18e66ed
omar <64643650+Omarr-kh@users.noreply.github.com>
2023-12-07 15:20:15 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,24 +24,22 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    '''Distribute an archive to our web servers'''
-
+    ''' deploy archive to servers '''
     try:
         if not os.path.isfile(archive_path):
             return False
-        file_path = archive_path.split('/')[-1]  # web_static_xxxxxx.tgz
-        name = file_path.split('.')[0]  # web_static_xxxxxx
-        r_path = '/data/web_static/releases'
-
-        put(archive_path, f'/tmp/{file_path}')
-        run(f'mkdir -p {r_path}/{name}/')
-        run(f'tar -xzf /tmp/{file_path} -C {r_path}/{name}/')
-        run(f'rm /tmp/{file_path}')
-        run(f'mv {r_path}/{name}/web_static/* {r_path}/{name}/')
-        run(f'rm -rf {r_path}/{name}/web_static')
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s {r_path}/{name}/ /data/web_static/current')
-        print('New version deployed!')
+        file_path = archive_path.split("/")[-1]
+        file_name = file_path.split(".")[0]
+        des_path = "/data/web_static/releases/"
+        put(archive_path, f"/tmp/{file_path}")
+        run(f"mkdir -p {des_path}/{file_name}")
+        run(f"tar -xzf /tmp/{file_path} -C {des_path}/{file_name}/")
+        run(f"rm /tmp/{file_path}")
+        run(f"rm -rf /data/web_static/current")
+        run(f'mv {des_path}/{file_name}/web_static/* {des_path}/{file_name}/')
+        run(f'rm -rf {des_path}/{file_name}/web_static')
+        run(f"ln -s {des_path}/{file_name}/ /data/web_static/current")
+        print("New version deployed!")
         return True
-    except Exception as e:
+    except Exception:
         return False
