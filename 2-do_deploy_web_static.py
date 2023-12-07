1ce1dea6d0271489b60747941917b200c4ce0304
omar <64643650+Omarr-kh@users.noreply.github.com>
2023-12-07 15:36:18 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,37 +9,22 @@ from datetime import datetime
 env.hosts = ["54.235.193.23", "	54.209.125.126"]
 
 
-def do_pack():
-    ''' pack the contents of web_static folder '''
-    current_date = datetime.now().strftime('%Y%m%d%H%M%S')
-    archive_name = "web_static_" + current_date + ".tgz"
-
-    local("mkdir -p versions")
-    folder_path = f"versions/{archive_name}"
-    try:
-        local(f"tar -cvzf {folder_path} web_static")
-        return folder_path
-    except Exception:
-        return None
-
-
 def do_deploy(archive_path):
     ''' deploy archive to servers '''
+    if exists(archive_path) is False:
+        return False
     try:
-        if not os.path.isfile(archive_path):
-            return False
         file_path = archive_path.split("/")[-1]
         file_name = file_path.split(".")[0]
         des_path = "/data/web_static/releases/"
-        put(archive_path, f"/tmp/{file_path}")
+        put(archive_path, f"/tmp/")
         run(f"mkdir -p {des_path}/{file_name}")
         run(f"tar -xzf /tmp/{file_path} -C {des_path}/{file_name}/")
         run(f"rm /tmp/{file_path}")
-        run(f"rm -rf /data/web_static/current")
         run(f'mv {des_path}/{file_name}/web_static/* {des_path}/{file_name}/')
         run(f'rm -rf {des_path}/{file_name}/web_static')
+        run(f"rm -rf /data/web_static/current")
         run(f"ln -s {des_path}/{file_name}/ /data/web_static/current")
-        print("New version deployed!")
         return True
     except Exception:
         return False
