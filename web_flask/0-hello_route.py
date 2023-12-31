f5126751311e36cc20b4408a40a1973beb38f27c
Omarr-kh <omar1752003@gmail.com>
2023-12-31 03:55:53 -0800

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' start a flask web app '''
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run()
