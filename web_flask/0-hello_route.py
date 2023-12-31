293ea7a2f6ecb9698dcbb5ef9589f3af7b72208f
Omarr-kh <omar1752003@gmail.com>
2023-12-31 04:01:37 -0800

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-''' start a flask web app '''
-import flask
+'''A minimal flask app'''
+from flask import Flask
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
-    ''' returns "Hello HBNB!" for the route "/" '''
-    return "Hello HBNB!"
+def home():
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
